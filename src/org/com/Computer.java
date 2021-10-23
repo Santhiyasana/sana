6288dc22ef12726008970fc64f9f43ba21bba236
Santhiyasana <santhiyakarthikeyan187@gmail.com>
2021-10-23 12:37:27 +0530

    sanaMsg

--- src/org/com/Computer.java
+++ src/org/com/Computer.java
@@ -1,6 +1,13 @@
 package org.com;
 
 public class Computer {
+	
+	
+	private void sana() {
+System.out.println("fctv");
+	}
+	
+	
 	public void computerModel() {
 		System.out.println("Computer Model - Ctch2356");
 
