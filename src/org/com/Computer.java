f93946d32c1fce29b1943bfa72b405efb0053803
Santhiyasana <santhiyakarthikeyan187@gmail.com>
2021-10-23 12:48:09 +0530

    joshMsg

--- src/org/com/Computer.java
+++ src/org/com/Computer.java
@@ -1,6 +1,11 @@
 package org.com;
 
 public class Computer {
+	
+	private void newtest() {
+System.out.println("jybtyt");
+	}
+	
 	public void computerModel() {
 		System.out.println("Computer Model - Ctch2356");
 
